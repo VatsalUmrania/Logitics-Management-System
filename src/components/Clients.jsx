43f313c908a5e2caa98f9a909c00ac4e6322abf5
Vatsal Umrania <vbumrania@gmail.com>
2025-05-29 17:22:20 +0530

    Clients Page Upadted

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -630,9 +630,6 @@ const ClientManagementPage = () => {
                       </td>
                       <td className="px-6 py-4">
                         <div className="flex items-center">
-                          <div className="w-10 h-10 bg-gradient-to-br from-purple-500 to-indigo-600 rounded-lg flex items-center justify-center mr-3">
-                            <Briefcase className="w-5 h-5 text-white" />
-                          </div>
                           <div>
                             <div className="text-sm font-medium text-gray-900">{client.name}</div>
                             <div className="text-xs text-gray-500">{client.industryType}</div>
